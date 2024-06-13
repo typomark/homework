51231d1d314cd6ce3a46e7aa2b64079f102e0fe4
Mark Gavrilyuk <mark.gvrlk@mail.ru>
2024-06-13 19:47:44 +0300

    complete homework

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let title = ("homework01");
+let window_screen = 50;
+let screenPrice = 10;
+let rollback = 15;
+let fullprice = 150;
+let adaptive = 75;
+
+alert("Hello World! This is my first homework");
+console.log("Hello! This is text for console");
+
+
