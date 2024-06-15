5f2b41e43500284e46000d872d69b4518748b980
Mark Gavrilyuk <mark.gvrlk@mail.ru>
2024-06-15 20:48:06 +0300

    homework02

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let title = ("homework01");
+/* let title = ("homework01");
 let window_screen = 50;
 let screenPrice = 10;
 let rollback = 15;
@@ -7,6 +7,31 @@ let fullprice = 150;
 let adaptive = 75;
 
 alert("Hello World! This is my first homework");
-console.log("Hello! This is text for console");
+console.log("Hello! This is text for console"); */
+
+let title = ('homework02');
+let screens = ('Простые, Сложные, Интерактивные');
+let screenPrice = 500;
+let screenPricedollar = 500;
+let rollback = 50;
+let fullPrice = 150000;
+let adaptive = 1 > 2;
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
 
 
+screenPricedollar *= 91;
+screenPrice *=1;
+
+console.log(screenPrice);
+console.log(screenPricedollar);
+
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+
+let otkat
+otkat = (fullPrice * (rollback / 100));
+console.log(otkat);
