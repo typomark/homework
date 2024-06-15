63a58e90506af568ca94619fdf5f66ee04e52f33
Mark Gavrilyuk <mark.gvrlk@mail.ru>
2024-06-15 19:46:18 +0300

    Выполнил ДЗ

---
+++ script.js
@@ -0,0 +1,26 @@
+let title = ('homework02');
+let screens = ('Простые, Сложные, Интерактивные');
+let screenPrice = 500;
+let screenPricedollar = 500;
+let rollback = 50;
+let fullPrice = 150000;
+let adaptive = 1 > 2;
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+
+
+screenPricedollar *= 91;
+screenPrice *=1;
+
+console.log(screenPrice);
+console.log(screenPricedollar);
+
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+
+let otkat
+otkat = (fullPrice * (rollback / 100));
+console.log(otkat);
